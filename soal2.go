56042d42d9438c2b008e97ed2cf512b3e02a0c76
I Komang Arjuna Tudung Negara <arjunatudung9@gmail.com>
2026-03-12 15:34:28 +0800

    Soal 2

--- soal2.go
+++ soal2.go
@@ -2,21 +2,24 @@ package main
 
 import "fmt"
 
+//Program Mengitung hasil dan peserta donasi
 func main() {
-	var (
-		satu, dua, tiga string
-		temp            string
-	)
-	fmt.Print("Masukan input string: ")
-	fmt.Scanln(&satu)
-	fmt.Print("Masukan input string: ")
-	fmt.Scanln(&dua)
-	fmt.Print("Masukan input string: ")
-	fmt.Scanln(&tiga)
-	fmt.Println("Output awal = " + satu + " " + dua + " " + tiga)
-	temp = satu
-	satu = dua
-	dua = tiga
-	tiga = temp
-	fmt.Println("Output akhir = " + satu + " " + dua + " " + tiga)
+	var w1, w2, w3, w4, acuan1, acuan2 string
+	var hasil bool
+	i := 1
+	hasil = true
+	fmt.Printf("Percobaan %d:", i)
+	fmt.Scan(&w1, &w2, &w3, &w4)
+	acuan1 = w1 + w2 + w3 + w4
+	for i = 2; i <= 5; i++ {
+		fmt.Printf("Percobaan %d:", i)
+		fmt.Scan(&w1, &w2, &w3, &w4)
+
+		acuan2 = w1 + w2 + w3 + w4
+		if acuan2 != acuan1 {
+			hasil = false
+		}
+	}
+	fmt.Print("Hasil :")
+	fmt.Print(hasil)
 }
