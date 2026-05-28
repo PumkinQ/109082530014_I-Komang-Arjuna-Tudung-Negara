192c2c58a043f37f6163691b08f8407066a01c69
I Komang Arjuna Tudung Negara <arjunatudung9@gmail.com>
2026-05-28 22:54:19 +0700

    upd soal 1

--- Modul 14/soal1.go
+++ Modul 14/soal1.go
@@ -2,33 +2,35 @@ package main
 
 import "fmt"
 
-func rumahkerabat(n int) {
-	var m, idx_min int
-	for i := 0; i < n; i++ {
-		var arr []int
-		fmt.Scan(&m)
-		arr = append(arr, m)
-		for j := 0; j < m; j++ {
-			fmt.Scan(arr[&j])
-			for x := 0; x < m-1; x++ {
-				idx_min := x
-				for y := x + 1; y < m; y++ {
-					if arr[y] < arr[idx_min] {
-						idx_min = y
-					}
-				}
-				arr[x], arr[idx_min] = arr[idx_min], arr[x]
-			}
-			for k := 0; k < m; k++ {
-				fmt.Print(arr[k], " ")
-			}
-			fmt.Println()
-		}
-	}
-}
-
 func main() {
-	var n int
+
+	var n, m, idx_min, temp int
 	fmt.Scan(&n)
-	rumahkerabat(n)
+	for i := 0; i < n; i++ {
+
+		fmt.Scan(&m)
+		var arr []int
+		for j := 0; j < m; j++ {
+
+			fmt.Scan(&temp)
+			arr = append(arr, temp)
+
+		}
+		for x := 0; x < m-1; x++ {
+			idx_min = x
+			for y := x + 1; y < m; y++ {
+				if arr[y] < arr[idx_min] {
+					idx_min = y
+				}
+			}
+			arr[x], arr[idx_min] = arr[idx_min], arr[x]
+		}
+		fmt.Println("HASIL Loop ke-", i+1)
+		for k := 0; k < m; k++ {
+			fmt.Print(arr[k], " ")
+		}
+		fmt.Println()
+		fmt.Println()
+	}
+
 }
